d566db2d8209c7f7a5579e59767a1dde0d1aa6ba
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-03-29 10:21:50 +0200

    inc fail

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,28 @@
-const {
-  Buffer,
-  constants: { MAX_LENGTH },
-} = require("buffer");
+// const {
+//   Buffer,
+//   constants: { MAX_LENGTH },
+// } = require("buffer");
 
-exports.sourceNodes = async () => {
-  const buffers = new Set();
-  let i = 0;
-  while (true) {
-    const buffer = Buffer.alloc(MAX_LENGTH, `x`);
-    buffers.add(buffer);
-    console.log(`round #${++i}`);
+// exports.sourceNodes = async () => {
+//   const buffers = new Set();
+//   let i = 0;
+//   while (true) {
+//     const buffer = Buffer.alloc(MAX_LENGTH, `x`);
+//     buffers.add(buffer);
+//     console.log(`round #${++i}`);
 
-    if (i % 10 === 0) {
-      await new Promise((resolve) => setImmediate(resolve));
-    }
+//     if (i % 10 === 0) {
+//       await new Promise((resolve) => setImmediate(resolve));
+//     }
+//   }
+// };
+
+let counter = 0;
+
+exports.createPages = () => {
+  counter++;
+
+  if (counter >= 2) {
+    throw new Error(`that should be inc build`);
   }
 };
